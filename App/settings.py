dc9f266f520cd9e4c80d29fafca1ffe9b8024a76
FrenzyWasHere <prathamkhanal962@gmail.com>
2025-01-22 12:31:23 +0545

    adding csrf trusted origins

--- App/settings.py
+++ App/settings.py
@@ -136,3 +136,4 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOW_ALL_ORIGINS = True
+CSRF_TRUSTED_ORIGINS = ["https://app-production-7b68.up.railway.app"]
