f01248c5f4e7204fd8340e967e7afddb704afdd3
FrenzyWasHere <prathamkhanal962@gmail.com>
2025-01-22 21:39:23 +0545

    urls

--- API/urls.py
+++ API/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('user-create/', views.userCreate, name="userCreate"),
     path('user-login/', views.userLogin, name='userLogin'),
     path('test-token/',views.testToken,name="testToken"),
-    path('user-update/',views.userUpdate,name="userUpdate"),
+    path('user-update/<str:pk>/',views.userUpdate,name="userUpdate"),
     path('user-delete/<str:pk>/',views.userDelete,name="userDelete"),
 ]
