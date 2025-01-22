f99dc41faac3dbe977951c42f76661b35cd93ea8
FrenzyWasHere <prathamkhanal962@gmail.com>
2025-01-22 14:09:10 +0545

    selecting field

--- API/serializers.py
+++ API/serializers.py
@@ -4,8 +4,6 @@ from django.contrib.auth.models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'first_name', 'last_name']
-        extra_kwargs = {
-            'password': {'write_only': True}
-        }
+        fields = '__all__'
+        
         
