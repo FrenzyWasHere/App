0c7b2dd13a962b75eb7f7f8e229f4f92659a6309
FrenzyWasHere <prathamkhanal962@gmail.com>
2025-01-25 19:09:15 +0545

    adding images

--- API/models.py
+++ API/models.py
@@ -1,3 +1,4 @@
+import os
 from django.db import models
 from django.contrib.auth.models import User
 import uuid
@@ -13,17 +14,30 @@ class Profile(models.Model):
     def __str__(self):
         return str(self.user)
 
+def get_doctor_image_upload_path(instance, filename):
+    # Return the original filename to prevent suffixes being added
+    return filename
+
 class Doctor(models.Model):
-    doctorId = models.UUIDField(default=uuid.uuid4, editable = False, unique = True, primary_key=True)
+    doctorId = models.UUIDField(default=uuid.uuid4, editable=False, unique=True, primary_key=True)
     doctorName = models.CharField(max_length=20, blank=False)
     doctorSpeciality = models.CharField(max_length=20, blank=False)
-    doctorExperience = models.CharField(max_length=20,blank=False)
-    doctorQualification = models.CharField(max_length=20,blank=False)
-    doctorImage = models.ImageField(default="default.jpg", blank=True)
-
+    doctorExperience = models.CharField(max_length=20, blank=False)
+    doctorQualification = models.CharField(max_length=20, blank=False)
+    doctorImage = models.ImageField(default="default.jpg", upload_to=get_doctor_image_upload_path, blank=True)
 
     def __str__(self):
-        return "Dr. " + self.doctorName+ " " + self.doctorSpeciality
+        return "Dr. " + self.doctorName + " " + self.doctorSpeciality
+
+    def save(self, *args, **kwargs):
+        if self.doctorImage:
+            # Get the original file name and set it if it's not the default
+            original_file_name = os.path.basename(self.doctorImage.name)
+            # Set the name back to the original
+            if self.doctorImage.name != "default.jpg":
+                self.doctorImage.name = original_file_name
+
+        super(Doctor, self).save(*args, **kwargs)
 
 
 
